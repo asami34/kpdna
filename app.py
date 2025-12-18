93641a826ab2e9288507e5a39dbff7506b991968
asami34 <asamikorkmaz18@gmail.com>
2025-12-18 15:30:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ FILENAME = 'https://docs.google.com/spreadsheets/d/1ch2PxWYFILX_hR6qE4sLKNjSq_Fg
 st.set_page_config(page_title="Genetics Visualization", layout="wide")
 
 
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=60)
 def load_data(path):
     try:
         df = pd.read_csv(path, engine='python', on_bad_lines='skip')
